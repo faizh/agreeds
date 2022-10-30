1a3a1ffe7b61c1dd232f396ea0a8a7cfb05e5283
faizhermawan_ <faizhermawan13@gmail.com>
2022-10-30 23:55:20 +0700

    add login and register page

--- application/views/layouts/v_header.php
+++ application/views/layouts/v_header.php
@@ -67,6 +67,11 @@
 									<div class="header-icons">
 										<a class="shopping-cart" href="cart.html"><i class="fas fa-shopping-cart"></i></a>
 										<a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-search"></i></a>
+										<a class="mobile-hide" href="#"><i class="fas fa-user"></i></a>
+									<ul class="sub-menu">
+										<li><a href="#">Faiz Hermawan</a></li>
+										<li><a href="#"><button class="btn btn-sm btn-danger">Logout</button></a></li>
+									</ul>
 									</div>
 								</li>
 							</ul>
